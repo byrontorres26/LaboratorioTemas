dcbaf2998e3911d95af4fe5c0e76b320790c9c7e
byrontorres26 <byrontorres@unitec.edu>
2026-08-30 22:50:57 -0600

    implementar pantallas y tab navigator

---
+++ MiAppTemas/App.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from './src/context/ThemeContext';
+import TabsNavigator from './src/navigation/TabsNavigator';
+export default function App() {
+  return (
+    <ThemeProvider>
+      <NavigationContainer>
+        <TabsNavigator />
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+}
